e174f411638b4950af09dc0a3783c28458ace4e1
vishvaRam <44122284+vishvaRam@users.noreply.github.com>
2019-06-25 11:52:37 +0100

    Enter

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
 module.exports = {
   MongoDBURL:
     "mongodb+srv://vishva:vishva@reactsocialnetwork-nwy95.mongodb.net/test?retryWrites=true&w=majority",
-  Token: "JsonWebToken"
+  SecretOrKey: "vishva"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 const User = require("../../Models/User");
+const jwt = require("jsonwebtoken");
 const gravator = require("gravator");
+const keys = require("../../config/key");
 
 // @route   api/user/test
 // @access  Public
@@ -52,13 +54,32 @@ router.post("/login", (req, res) => {
 
   User.findOne({ email }).then(user => {
     if (!user) {
+      // Checking the User in DB
       return res.status(404).json({ message: "User not found" });
     }
     bcrypt.compare(password, user.password).then(isMatch => {
       if (!isMatch) {
+        // Checking the Password
         return res.status(404).json({ password: "Password is incorrect" });
       } else {
-        res.status(200).json({ message: "you are logged in " });
+        // Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+        // creating JWT Token
+        jwt.sign(
+          payload,
+          keys.SecretOrKey,
+          { expiresIn: 345600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "bearer " + token
+            });
+          }
+        );
       }
     });
   });
